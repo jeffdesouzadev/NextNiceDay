f01ea74c428f7fb62543d4d5480af93bde8dd63b
jeffdesouzadev <76910986+jeffdesouzadev@users.noreply.github.com>
2021-02-25 15:45:46 -0600

    Update TWILIO_AUTH.php

--- TWILIO_AUTH.php
+++ TWILIO_AUTH.php
@@ -1,4 +1,5 @@
 <?php
-$TWILIO_SID = "AC7cbd83c363a1a7b042046ff55df09c17";
-$TWILIO_AUTH = "cedcba7e6a1a384177cbaafe8ec2ed3a";
+//please update with your SID and AUTH from Twilio!
+$TWILIO_SID = "BD7cbd83c363a1a7b042046ff55df09c17";
+$TWILIO_AUTH = "dfdcba7e6a1a384177cbaafe8ec2ed3a";
 ?>
