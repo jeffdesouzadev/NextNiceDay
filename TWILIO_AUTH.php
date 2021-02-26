18b4316cbda5976e3c00a8acdbdc839d181792a8
jeffdesouzadev <jeffdesouzadev@gmail.com>
2021-02-26 01:25:30 -0600

    quick and dirty repair - new AUTH tokens due to first (and hopefully last) accidental upload to git

--- TWILIO_AUTH.php
+++ TWILIO_AUTH.php
@@ -1,6 +1,8 @@
 <?php
-//please update with your SID and AUTH from Twilio (these are fake, of course)!
+//please update with your SID, AUTH, AND NUMBER from Twilio (these are fake, of course)!
 //http://www.twilio.com
 $TWILIO_SID = "BD7cbd83c363a1a7b042046ff55df09c17";
-$TWILIO_AUTH = "dfdcba7e6a1a384177cbaafe8ec2ed3a";
+$TWILIO_PHONE_NUMBER = "5125551234";
+$TWILIO_AUTH = "REFzZDAwZGY5MDQwPLRhMmMxMzY4NWJhY2U0MGM5NjgxZjpiYTMwZDk2MzA1Y2YzMWRiMmM1ZTQ0MzYyNjZlYTJkMA==";
+
 ?>
